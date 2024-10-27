b5aa1743c1d556f61ac941c6ae21e3a8860c6c42
fqhhusain <88548292+fqhhusain@users.noreply.github.com>
2024-10-27 22:22:38 +0700

    Create Script10_Colossal.sh

---
+++ script/Script10_Colossal.sh
@@ -0,0 +1,35 @@
+mkdir -p /etc/nginx/supersecret
+htpasswd -b -c /etc/nginx/supersecret/htpasswd arminannie jrkmit14
+
+cp /etc/nginx/sites-available/default /etc/nginx/sites-available/lb_php
+
+echo ' upstream worker {
+        #least_conn;
+        #ip_hash;
+    server 192.240.2.2;
+    server 192.240.2.3;
+    server 192.240.2.4;
+}
+
+server {
+    listen 80;
+    server_name eldia.it14.com www.eldia.it14.com;
+
+    root /var/www/html;
+
+    index index.html index.htm index.nginx-debian.html index.php;
+
+    server_name _;
+
+    location / {
+        proxy_pass http://worker;
+    }
+
+    auth_basic "Restricted Content";
+    auth_basic_user_file /etc/nginx/supersecret/htpasswd;
+} ' > /etc/nginx/sites-available/lb_php
+
+ln -s /etc/nginx/sites-available/lb_php /etc/nginx/sites-enabled/
+rm /etc/nginx/sites-enabled/default
+
+service nginx restart
