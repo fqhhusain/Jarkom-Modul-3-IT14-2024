485ebc47164025f3bc255f76434a52ded7de6e53
fqhhusain <88548292+fqhhusain@users.noreply.github.com>
2024-10-27 22:17:36 +0700

    Create Script_awal_PHPWorker.sh

---
+++ script/Script_awal_PHPWorker.sh
@@ -0,0 +1,5 @@
+echo 'nameserver 192.240.4.3' > /etc/resolv.conf   # DNS Server 
+apt-get update
+apt-get install nginx -y
+apt-get install wget unzip -y
+apt-get install php7.3-fpm php7.3-common php7.3-mysql php7.3-gmp php7.3-curl php7.3-intl php7.3-mbstring php7.3-xmlrpc php7.3-gd php7.3-xml php7.3-cli php7.3-zip -y
